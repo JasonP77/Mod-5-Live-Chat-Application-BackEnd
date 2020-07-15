6f383af4ccf7a99fd6a605e4e7b245f08ba377c8
JasonP77 <llvslr77@gmail.com>
2020-07-15 13:16:11 -0400

    comments for what received method does in the rooms_channel.rb

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -6,9 +6,10 @@ class RoomsChannel < ApplicationCable::Channel
     stream_for @room
 	end
 
+  # this method takes care of all the users and messages that are sent and received in the chatroom.
+  
    def received(data)
     RoomsChannel.broadcast_to(@room, {room: @room, user: @room.users, messages: @room.messages})
-    byebug
   end
 	
 end
