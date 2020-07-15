1e8911d873da69f347c87c40f61e2df31bf0fdea
JasonP77 <llvslr77@gmail.com>
2020-07-15 13:14:24 -0400

    comment for chatrooms method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
 	has_many :messages, through: :chatrooms
 
+	# this method returns all chatrooms belong to specific user
 	def chatrooms
 		Chatroom.where(user_id: self.id ).or(Chatroom.where(friend_id: self.id))
 	end
