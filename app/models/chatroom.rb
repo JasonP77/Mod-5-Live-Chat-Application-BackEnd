7c7eda5ca1979e261ecd97fe2210e2decbce6f35
JasonP77 <llvslr77@gmail.com>
2020-07-15 13:13:28 -0400

    comments for method override

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,6 +3,8 @@ class Chatroom < ApplicationRecord
 	belongs_to :friend, foreign_key: "friend_id", class_name: "User"
 	has_many :messages
 
+	# this method overrides users method and returns all participating users in specific chatroom.
+	
 	def users
 		User.where(id: self.user_id).or(User.where(id: self.friend_id))
 	end
