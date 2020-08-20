c7e61e6f25bd18f8ada960c9d6c619ceaa1f761a
JasonP77 <llvslr77@gmail.com>
2020-08-20 16:39:41 -0400

    friendship relationship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,6 +6,7 @@ class FriendshipsController < ApplicationController
 	end
 
 	# this method creates a relationship between two diffrent users
+	# it also shows all friend relationships upon creation
 def create 
 		user = User.create(username: params[:username], password: params[:password], profile_img: nil)
 		friendship = Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id])
