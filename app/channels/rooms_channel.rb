d6fdc51eb7552e954956f70dbd03bc40d680d7d0
JasonP77 <llvslr77@gmail.com>
2020-08-27 15:49:41 -0400

    receving data in rooms_channel.rb

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -8,6 +8,7 @@ class RoomsChannel < ApplicationCable::Channel
 
   # this method takes care of all the users and messages that are sent and received in the chatroom.
   # all of the data needs to be in right format in order to make it work properly.
+  # check front-end side files to see which data is passed.
    def received(data)
     RoomsChannel.broadcast_to(@room, {room: @room, user: @room.users, messages: @room.messages})
   end
