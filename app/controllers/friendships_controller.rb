b62ae713b6a0a5a42509a04850a0b675f55b4cf3
JasonP77 <llvslr77@gmail.com>
2020-09-03 16:03:12 -0400

    friendships specific render

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,6 +7,7 @@ class FriendshipsController < ApplicationController
 
 	# this method creates a relationship between two diffrent users
 	# it also shows all friend relationships upon creation
+	# make sure to specific what needs to be rendered in "render json:"
 def create 
 		user = User.create(username: params[:username], password: params[:password], profile_img: nil)
 		friendship = Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id])
